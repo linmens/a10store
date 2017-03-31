805cd95beb6af4cca4cd2b94f22d9f2ab9c7c91f
linmens <278604142@qq.com>
2017-03-31 13:59:14 +0800

    Update loginCtrl.js

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -2,7 +2,7 @@ define(function (require) {
     var app = require('../../app');
     app.controller('loginCtrl', function ($scope, $http, ipCookie, $timeout,$state,$rootScope) {
         $scope.loginWeb = function () {
-            $http.post('app/login/api/login.php', {
+            $http.post('http://yibu.a10store.com/app/login/api/login.php', {
                 username: $scope.credentials.username,
                 password: $scope.credentials.password
             }).success(function (data) {
